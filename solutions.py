7ba6d5e8c848d65732f217e29a2acb005567d0ee
Luka Goderdzishvili <lukagoderdzishvili4@gmail.com>
2024-11-15 11:48:09 +0400

    Improbed twosum method

--- solutions.py
+++ solutions.py
@@ -1,9 +1,11 @@
-class TwoSum(object):
+class Solution(object):
     def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[j] == target - nums[i]:
-                    return [i,j]
+       data = {}
+       for i, num in enumerate(nums):
+            diff = target - num
+            if diff in data:
+                return [data[diff], i]
+            data[num] = i
 
 class IsPalindrome(object):
     def isPalindrome(self, x):
